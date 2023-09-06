a76ca69ff4808785f9bd516b47e912070fe94646
oleksandr.liannoi <o.liannoi@pushkastudios.com>
2023-09-06 18:05:34 +0300

    Simple alloc-concerned mem functions.

--- VGE/Source/Memory.h
+++ VGE/Source/Memory.h
@@ -6,4 +6,13 @@ namespace vge::memory
 	inline void memmove(void* dst, void* src, size_t size)	{ std::memmove(dst, src, size); }
 	inline void memset (void* dst, int32 val, size_t size)	{ std::memset(dst, val, size); }
 	inline void memzero(void* dst, size_t size)				{ std::memset(dst, 0, size); }
+
+	template<typename T, uint32 Count = 1>
+	inline T* allocate() { static_assert(Count > 0); return reinterpret_cast<T*>(::operator new(sizeof(T) * Count)); }
+
+	template<typename T>
+	inline void free(T*& data) { ::operator delete(data); data = nullptr; }
+
+	template<typename T, typename... Args>
+	inline T* emplace(T* where, Args... args) { return new (where) T(std::forward<Args>(args)...); }
 }
